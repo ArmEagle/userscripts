9866dddff96858aeb4d5214f0be42e7dbb370f3e
ArmEagle <ArmEagle@users.noreply.github.com>
2018-04-08 15:43:03 +0200

    Create select2search.js

---
+++ util/select2search.js
@@ -0,0 +1,20 @@
+(function($){
+  /**
+   * Search and select an option of a select2 element. Supports ajax backed data.
+   * @param {string} query
+   *  Option value to search for
+   * @param {string} property (optional, defaults to "name")
+   *  Property of data to compare `query` with.
+   */
+  $.fn.select2search = function(query, property) {
+    var property = (typeof property === "undefined") ? "name" : property;
+    function itemsLoadedHandler(event) {
+      console.log(event);
+      var data = event.items.results.find(data => data[property] === query);
+      this.select2("data", data);
+      this.select2("close");
+    }
+    this.one("select2-loaded", itemsLoadedHandler.bind(this));
+    this.select2("search", query);
+  }
+}(jQuery));
